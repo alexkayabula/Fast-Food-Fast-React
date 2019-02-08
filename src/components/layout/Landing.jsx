8be356ab6ffd10e861db19c3e674aee556a90cca
Kayabula Alex Joseph <kayabulaalexjoseph@ALEX.local>
2019-02-08 10:50:24 +0300

    chore(react router v4): setup react router version 4 - add react router. - configure App.js
    [Finishes #163804239]

---
+++ src/components/layout/Landing.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+/**
+ * Renders the landing page.
+ */
+
+class Landing extends Component {
+  render() {
+    return (
+      <section id="landing-page" className="flex-grow-1">
+        <section id="featured-welcome">
+          <div className="home-inner container">
+            <div className="row">
+              <div className="col-lg-8 d-none d-lg-block marg-lg-zeus">
+                <h1 className="display-4 mt-4" >
+                Welcome To Fast Food Fast, <br />
+                Your Favourite Meal Is Just A Click Away
+                </h1>
+              </div>
+
+              <div className="col-lg-4 my-5">
+                
+              </div>
+            </div>
+          </div>
+        </section>
+      </section>
+    );
+  }
+}
+ 
+export default Landing;
