c3a935316e4762fbbdb193a391132121f07308ab
Kayabula Alex Joseph <kayabulaalexjoseph@ALEX.local>
2019-02-08 10:50:24 +0300

    chore(react router v4): setup react router version 4 - add react router. - configure App.js
    [Finishes #163804239]

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 import './App.scss';
+import Landing from './components/layout/Landing';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Fast Food Fast</h1>
-      </div>
+      <Router>
+        <div className="App d-flex flex-column">
+          <Switch>
+            <Route exact path="/" component={Landing} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
